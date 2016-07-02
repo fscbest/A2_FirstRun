4cc04e1b91b0807d2ebba3fbe65535298ac0d019
fscbest <romangeyber@gmail.com>
2016-07-02 15:14:01 +0300

    .

--- app/observabble.component.ts
+++ app/observabble.component.ts
@@ -1,4 +1,9 @@
 import {Component} from 'angular2/core'
+import {ControlGroup, FormBuilder} from "angular2/common";
+import {Observable} from "../node_modules/rxjs/Observable";
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/filter';
 
 @Component({
     selector: 'obbserv',
@@ -6,5 +11,17 @@ import {Component} from 'angular2/core'
 })
 
 export class ObbserableComponent {
+    form:ControlGroup;
 
+    constructor(fb: FormBuilder){
+        this.form = fb.group({
+            search: []
+        });
+
+        var search = this.form.find('search');
+        search.valueChanges
+            .debounceTime(400)
+            .map(str => (<string>str).replace(' ', '-'))
+            .subscribe(x => console.log(x));
+    }
 }
